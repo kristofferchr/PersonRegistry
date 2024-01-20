10190428b5edf0bc4c22468df7f56c5453b8258d
Kristoffer Moberg Christensen <KristofferMoberg.Christensen@skatteetaten.no>
2024-01-20 20:18:59 +0100

    feat: init backend

---
+++ backend/src/test/kotlin/dev/kristofferchr/personlist/PersonlistApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.kristofferchr.personlist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PersonlistApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
