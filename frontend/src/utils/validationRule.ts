f69541fc555ba06155609c00acb7c978af4de666
Kristoffer Moberg Christensen <KristofferMoberg.Christensen@skatteetaten.no>
2024-01-22 23:12:34 +0100

    fix: implicit any for v

--- frontend/src/utils/validationRule.ts
+++ frontend/src/utils/validationRule.ts
@@ -1,12 +1,12 @@
 export const nameRules = [
-  v => !!v || 'Navn er påkrevd',
-  v => /^[A-Z a-z]+$/.test(v) || 'Kun bokstaver som er gyldig',
+  (v: any) => !!v || 'Navn er påkrevd',
+  (v: any) => /^[A-Z a-z]+$/.test(v) || 'Kun bokstaver som er gyldig',
 ]
 
 export const ageRules = [
-  v => !!v || 'Alder er påkrevd',
-  v => /^\d+$/.test(v) || 'Kun tall er lovlig',
-  v => {
+  (v: any) => !!v || 'Alder er påkrevd',
+  (v: any) => /^\d+$/.test(v) || 'Kun tall er lovlig',
+  (v: any) => {
     if (v >= 0 && v <= 200) return true;
 
     return 'Alder kan ikke være negativ og må være mindre enn 200';
